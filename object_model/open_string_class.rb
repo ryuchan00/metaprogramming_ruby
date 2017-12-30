4be844f900cfa8c8cae0914dc0e4e10f2b6bc319
ryuchun00 <leokun0210@gmail.com>
2017-12-31 01:12:25 +0900

    rubyの標準classの拡張にイメージが湧かなかったので、簡単な拡張の実験を行った・

---
+++ object_model/open_string_class.rb
@@ -0,0 +1,9 @@
+# String classの拡張の実験のため
+# ref: https://qiita.com/kidach1/items/b1672f1c16e2d15f2d9c
+class String
+  def love_ruby
+    self + ' I love Ruby!'
+  end
+end
+
+p 'My name is kidachi.'.love_ruby
