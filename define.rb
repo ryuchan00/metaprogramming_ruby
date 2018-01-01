ea08018e3e97a0f822aecf374f679a66b7bd2ab4
ryuchun00 <leokun0210@gmail.com>
2018-01-01 12:33:42 +0900

    定数のネストを確認した

--- define.rb
+++ define.rb
@@ -1,9 +1,24 @@
+require 'pry'
+
 module M
   class C
     X = 'ある定数'
+    module M2
+      p Module.nesting
+    end
   end
 
   p C::X
 end
 
 p M::C::X
+
+Y = 'ルートレベルの定数'
+
+module M
+  Y = 'Mにある定数'
+  p Y
+  p ::Y
+  binding.pry
+end
+
