623335cf3381772957d2955cb17071fb8a25ea56
ryuchan00 <leokun0210@gmail.com>
2018-09-11 09:19:34 +0900

    えーフラットスコーブ実現のために、define_methodを使用するのか....

--- blocks/flat_scope_2.rb
+++ blocks/flat_scope_2.rb
@@ -1,11 +1,13 @@
 my_var = "成功"
+
 MyClass = Class.new do
   # これでmy_varを表示できる
   puts "クラス定義の中は#{my_var} !"
 
   def my_method
     puts "... でもここに表示するには"
-    # ... でもここに表示するには
+    # puts "メソッド定義の中は#{my_var} !"
+    # これは無理
   end
 end
 
--- blocks/flat_scope_3.rb
+++ blocks/flat_scope_3.rb
@@ -1,10 +1,10 @@
 my_var = "成功"
 
 MyClass = Class.new do
-  puts "クラス定義の中は #{my_var} !"
+  puts "クラス定義の中は#{my_var}!"
 
   define_method :my_method do
-    puts "メソッド定義のなかも #{my_var} !"
+    "メソッド定義の中も#{my_var}!"
   end
 end
 
